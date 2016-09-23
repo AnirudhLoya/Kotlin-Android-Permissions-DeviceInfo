e98a8607eb39514f8748aa3a3fe147fcff5ea3f9
Anirudh Loya <loya.anirudh@gmail.com>
2016-09-23 13:53:55 +0530

    Using Permissions Activity.

--- app/src/main/java/io/vrinda/kotlinandroidpermissions/MainActivity.kt
+++ app/src/main/java/io/vrinda/kotlinandroidpermissions/MainActivity.kt
@@ -1,13 +1,44 @@
 package io.vrinda.kotlinandroidpermissions
 
+import android.Manifest
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import io.vrinda.kotlinpermissions.PermissionCallBack
+import io.vrinda.kotlinpermissions.PermissionsActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : PermissionsActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-    }
+        callPermission.setOnClickListener {
+            requestPermissions(Manifest.permission.CALL_PHONE, object : PermissionCallBack {
+                override fun permissionGranted() {
+                    super.permissionGranted()
+                    Log.v("Call permissions", "Granted")
+                }
 
+                override fun permissionDenied() {
+                    super.permissionDenied()
+                    Log.v("Call permissions", "Denied")
+                }
+            })
+        }
+        cameraPermission.setOnClickListener {
+            requestPermissions(arrayOf(android.Manifest.permission.CAMERA, android.Manifest.permission.WRITE_EXTERNAL_STORAGE), object : PermissionCallBack {
+                override fun permissionGranted() {
+                    super.permissionGranted()
+                    Log.v("Camera permissions", "Denied")
+                }
+
+                override fun permissionDenied() {
+                    super.permissionDenied()
+                    Log.v("Camera permissions", "Denied")
+                }
+            })
+        }
+
+    }
 }
+
